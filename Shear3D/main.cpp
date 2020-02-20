94f0bc001df82cac0d86348701b66c00fe5e22dd
42yeah <potion@live.cn>
2020-02-20 16:43:16 +0800

    Initial Commit

---
+++ Shear3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Shear3D
+//
+//  Created by 周昊 on 2020/2/20.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
